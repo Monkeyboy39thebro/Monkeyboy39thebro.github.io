0811dd402bb6faef624e737436cf6a1b3a817d21
Monkeyboy39thebro <galvanfer514@gmail.com>
2026-01-21 09:27:26 -0500

    push

---
+++ tammusic.js
@@ -0,0 +1,19 @@
+var music = document.getElementById("javaAudio")
+
+		function playMusic(){
+			music.play();
+
+		}
+
+		function stopMusic(){
+			music.pause();
+
+		}
+
+		function changeImage(){
+			document.getElementById("javaImage").src="tam.jpg"
+
+		}
+
+		
+		
